af3857e557ce1efd9aa8a8f7e53295afabb2238f
Dmitry Titov <diman2012.titov@yandex.ru>
2023-06-14 19:55:41 +0300

    test3

--- client/config/index.js
+++ client/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'list_books/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
